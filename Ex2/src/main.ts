630f900cf06ab114c77ca7544aec484005538bcc
pgr3931 <45991112+pgr3931@users.noreply.github.com>
2020-03-13 16:32:02 +0100

    Added correct commands

--- Ex2/src/main.ts
+++ Ex2/src/main.ts
@@ -1,5 +1,5 @@
 import sum from "./sum";
 import square from "./square";
 
-document.writeln('The sum of 5 and 8 is ', sum(5, 8).toString(), '<br>');
+document.writeln('The sum of 5 and 8 is ', sum(5, 10).toString(), '<br>');
 document.writeln('The square of 6 is ', square(6).toString(), '<b>');
