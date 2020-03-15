8a1b516ac4c84922f15ba891a8b5621c52b860c0
pgr3931 <45991112+pgr3931@users.noreply.github.com>
2020-03-15 11:05:06 +0100

    Ex3 setup

---
+++ Ex3/src/main.ts
@@ -0,0 +1,5 @@
+import sum from "./sum";
+import square from "./square";
+
+document.writeln('The sum of 5 and 8 is ', sum(5, 10).toString(), '<br>');
+document.writeln('The square of 6 is ', square(6).toString(), '<b>');
