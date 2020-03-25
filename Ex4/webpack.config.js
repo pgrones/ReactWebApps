b5e2d2c0f577676e87a84d28ad748128ea2ab582
pgr3931 <45991112+pgr3931@users.noreply.github.com>
2020-03-25 16:30:38 +0100

    Setup Ex4

---
+++ Ex4/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.tsx',
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    mode:'development',
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : 'src/index.html',
+            filename: "./index.html"
+        })
+    ]
+
+};
